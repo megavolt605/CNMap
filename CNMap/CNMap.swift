9d87ce862527993870ea09e971cc0c65a47f95e1
Igor Smirnov <dev_ios@ronin.pro>
2016-10-08 16:03:22 +0300

    add CNMap stub class

---
+++ CNMap/CNMap.swift
@@ -0,0 +1,13 @@
+//
+//  CNMap.swift
+//  CNMap
+//
+//  Created by Igor Smirnov on 08/10/2016.
+//  Copyright © 2016 Complex Numbers. All rights reserved.
+//
+
+import Foundation
+
+class CNMap {
+    
+}
